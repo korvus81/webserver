5054d89583fbf34b9d2db35444cadb58ae200390
Jeff Poole <jpoole@getjive.com>
2015-09-09 22:40:12 -0600

    Initial commit

---
+++ src/test/java/net/jeffpoole/WebserverMainTest.java
@@ -0,0 +1,23 @@
+package net.jeffpoole;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class WebserverMainTest
+{
+  @Before
+  public void setup() throws Exception
+  {
+
+  }
+
+  @Test
+  public void testStartup() throws Exception
+  {
+
+  }
+}
